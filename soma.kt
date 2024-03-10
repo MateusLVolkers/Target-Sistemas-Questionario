586458b08a8488a995eb8ddc2359770ae8f56381
MateusLVolkers <46768661+MateusLVolkers@users.noreply.github.com>
2024-03-10 02:05:28 -0300

    Create soma.kt

---
+++ soma.kt
@@ -0,0 +1,14 @@
+// O código foi desenvolvido na linguagem kotlin e pode ser testado em https://play.kotlinlang.org/
+
+fun main() {
+    
+    var k = 0
+    var soma = 0
+    val indice = 13
+    
+    while (k < indice) {
+        k += 1
+        soma += k
+        println(soma)
+    }
+}
